9137b267c9ee14672db0ae994697e54bdf1b6a5d
tauapp <ummo2564@student.harmonytx.org>
2021-12-07 16:40:12 +0000

    I Continue to suffer

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# harmony-nft
+# Harmony Rocketry NFT Platform
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/tauapp/harmony-nft)
+
 A site to buy and sell NFTs as a fundraiser for the Harmony Rocketry Club.
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIconModule } from '@angular/material/icon'
 import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 
 @NgModule({
@@ -20,7 +22,8 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
