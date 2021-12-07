fa339d8d774f323789f3b3a64e2e3359f1516619
tauapp <ummo2564@student.harmonytx.org>
2021-12-07 16:49:08 +0000

    More things

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIconModule } from '@angular/material/icon'
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 
 
@@ -23,7 +24,8 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
