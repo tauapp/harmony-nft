8a864392aebaf2ee4cc1f01a49872007f2140bb0
tauapp <ummo2564@student.harmonytx.org>
2021-12-10 14:18:08 +0000

    Login

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+interface User {
+  username: string,
+  key: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  //TODO: Make it pull current user from cookies.
+  currentUser = new BehaviorSubject<User | undefined> (undefined)
 
   constructor() { }
 }
